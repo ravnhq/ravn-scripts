79f6a37908cec6c1b65cfe40a4b710b6c192a4aa
Donovan Hiland <donovan.hiland@gmail.com>
2019-07-01 16:28:59 -0600

    fix missing eslint dependency

--- packages/expo-template-typescript/.eslintrc.js
+++ packages/expo-template-typescript/.eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: [
-    '@ravn-dev/eslint-config-ravn/typescript',
+    '@ravn-dev/eslint-config-ravn/graphql',
     '@ravn-dev/eslint-config-ravn/react',
     '@ravn-dev/eslint-config-ravn/react-native',
     '@ravn-dev/eslint-config-ravn/jest',
-    '@ravn-dev/eslint-config-ravn/graphql',
+    '@ravn-dev/eslint-config-ravn/typescript',
     '@ravn-dev/eslint-config-ravn/base',
   ],
 }
