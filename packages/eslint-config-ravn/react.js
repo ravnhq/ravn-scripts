3f1e881308051de1f3cc2e811ac9911601a8eca0
Donovan Hiland <donovan.hiland@gmail.com>
2020-03-23 17:17:04 -0600

    update lint configs

--- packages/eslint-config-ravn/react.js
+++ packages/eslint-config-ravn/react.js
@@ -7,11 +7,13 @@ module.exports = {
   },
   plugins: ['react', 'react-hooks'],
   rules: {
-    'global-require': 'error',
-
     'react/jsx-boolean-value': 'error',
-    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', '.tsx'] },
+    ],
     'react/no-did-mount-set-state': 'off', // https://github.com/yannickcr/eslint-plugin-react/issues/1754
+    'react/no-unescaped-entities': 'off',
     'react/jsx-curly-brace-presence': 'error',
 
     'react-hooks/rules-of-hooks': 'error',
--- packages/eslint-config-ravn/typescript.js
+++ packages/eslint-config-ravn/typescript.js
@@ -7,11 +7,15 @@ module.exports = {
     project: 'tsconfig.json',
   },
   rules: {
-    'react/prop-types': 'off',
     'consistent-return': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    'global-require': 'error',
+    'react/prop-types': 'off',
+    'require-await': 'off',
+
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/require-await': 'error',
   },
 }
